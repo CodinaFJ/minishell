862f87e3f7ee2946f8a01e97e84e5f9cecdd95e0
jcodina-Ubuntu <jcodina-@student.42madrid.com>
2024-01-19 19:22:04 +0100

    Dummy structure

---
+++ include/minishell.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcodina- <jcodina-@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/19 19:16:43 by jcodina-          #+#    #+#             */
+/*   Updated: 2024/01/19 19:18:12 by jcodina-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+#include "../lib/libft/include/libft.h"
+#include "../lib/data_structures_C/include/data_structures.h"
+
+
+#endif
