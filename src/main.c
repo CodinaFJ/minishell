86b9811af084a3149c7e3e07164062b2b96b5a2b
jcodina- <fjavier.codina@gmail.com>
2024-01-21 12:47:01 +0100

    Dummy shell prompt

--- src/main.c
+++ src/main.c
@@ -6,14 +6,44 @@
 /*   By: jcodina- <jcodina-@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/19 19:31:07 by jcodina-          #+#    #+#             */
-/*   Updated: 2024/01/19 19:32:16 by jcodina-         ###   ########.fr       */
+/*   Updated: 2024/01/21 12:44:18 by jcodina-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
+void	do_minishell(char **tokens)
+{
+	ft_strs_free(tokens);
+}
+
+char	**tokenize(char *str)
+{
+	return (ft_split(str, ' '));
+}
+
+t_bool	assert_input(char *input)
+{
+	if (input == NULL)
+		return (false);
+	return (true);
+}
+
 int	main(void)
 {
-	ft_printf("Hello minishell\n");
+	char *str;
+	
+	while (1)
+	{
+		ft_printf("BashCrandicoot> ");
+		str = get_next_line(0);
+		if (!assert_input(str))
+		{
+			free(str);
+			continue ;
+		}
+		do_minishell(tokenize(str));
+		free(str);
+	}
 	return (0);
 }
