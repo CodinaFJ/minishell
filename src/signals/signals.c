743bd984adc6bdc25328a00a6b90aa0cdb42a0f4
jcodina-Ubuntu <jcodina-@student.42madrid.com>
2024-02-01 22:06:42 +0100

    Add basic signal handling for CTRL C and CTRL \
    Now CTRL C only prompts a new line. Is done just by
    printing \n and the minishell prompt, but this is not
    the correct way of doing it.
    
    TODO: interrupt the active process and restart the asking
    for a command state.

---
+++ src/signals/signals.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcodina- <fjavier.codina@gmail.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 15:04:27 by jcodina-          #+#    #+#             */
+/*   Updated: 2024/02/01 21:47:15 by jcodina-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "signals.h"
+
+void	handle_signal_sigint(void)
+{
+	ft_printf("\n");
+	ft_printf(SHELL_PROMT);
+}
+
+void	handle_signal_sigquit(void)
+{
+
+}
+
+void signal_handler(int signum, siginfo_t *info, void *context)
+{
+	(void) context;
+	(void) info;
+	if (signum == SIGINT)
+		handle_signal_sigint();
+	else if (signum == SIGQUIT)
+		handle_signal_sigquit();
+}
+
+static void	register_sig_handlers(void)
+{
+	struct sigaction	sa;
+
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = SA_RESTART | SA_SIGINFO;
+	sa.sa_sigaction = signal_handler;
+	sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGQUIT, &sa, NULL);
+}
+
+void signals_init(void)
+{
+	register_sig_handlers();
+}
---
+++ src/signals/signals.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   signals.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jcodina- <fjavier.codina@gmail.com>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/01 15:02:54 by jcodina-          #+#    #+#             */
+/*   Updated: 2024/02/01 21:49:39 by jcodina-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SIGNALS_H
+# define SIGNALS_H
+# include "../../lib/libft/include/libft.h"
+# include "../../include/minishell.h"
+# include <signal.h>
+
+void 	signals_init(void);
+
+#endif
